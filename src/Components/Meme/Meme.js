9b146a5e5b72dec3efad4cee495ffbe4a3adadca
Marlon Leon <ghostveene@gmail.com>
2023-01-20 10:01:17 -0500

    changed classname and removed form element

--- src/Components/Meme/Meme.js
+++ src/Components/Meme/Meme.js
@@ -4,14 +4,14 @@ import "./Meme.css"
 
 export default function Meme() {
     return (
-        <form className="meme-container">
-            <div className="meme-input-section">
+        <div className="form">
+            <div className="meme-container">
                 <input type="text" placeholder="Top text" className="meme-text-field" />
                 <input type="text" placeholder="Bottom text" className="meme-text-field" />
                 <button className="meme-btn">Get a new meme image 
                     <AiOutlinePicture className="btn-icon"/>
                 </button>
             </div>
-        </form>
+        </div>
     )
 }
