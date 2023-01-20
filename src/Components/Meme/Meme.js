9ce5bdc8135d987cddf8ab308074a9934347cc61
Marlon Leon <ghostveene@gmail.com>
2023-01-20 09:28:01 -0500

    gave new classnames and div

--- src/Components/Meme/Meme.js
+++ src/Components/Meme/Meme.js
@@ -5,11 +5,13 @@ import "./Meme.css"
 export default function Meme() {
     return (
         <form className="meme-container">
-            <input type="text" placeholder="Shut up" className="meme-text-field"></input>
-            <input type="text" placeholder="and take my money" className="meme-text-field"></input>
-            <button type="submit" className="meme-btn">Get a new meme image 
-                <AiOutlinePicture className="btn-icon"/>
-            </button>
+            <div className="meme-input-section">
+                <input type="text" placeholder="Shut up" className="meme-text-field" />
+                <input type="text" placeholder="and take my money" className="meme-text-field" />
+                <button type="submit" className="meme-btn">Get a new meme image 
+                    <AiOutlinePicture className="btn-icon"/>
+                </button>
+            </div>
         </form>
     )
 }
