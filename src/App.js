1817b872c13ce20929d303d2efd0ea5e2b44d133
Marlon Leon <ghostveene@gmail.com>
2023-01-20 07:10:37 -0500

    importing header and form, may need another component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Header from "./Components/Header/Header"
+import Form from "./Components/Form/Form"
+
 export default function App() {
   return (
     <div className="all-content">
+      <Header />
+      <Form />
     </div>
   );
 }
