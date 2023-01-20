31bdb08308e491668b3ca97ffe9e5ce85506ad70
Marlon Leon <ghostveene@gmail.com>
2023-01-20 09:42:30 -0500

    added new placeholder

--- src/Components/Meme/Meme.js
+++ src/Components/Meme/Meme.js
@@ -6,8 +6,8 @@ export default function Meme() {
     return (
         <form className="meme-container">
             <div className="meme-input-section">
-                <input type="text" placeholder="Shut up" className="meme-text-field" />
-                <input type="text" placeholder="and take my money" className="meme-text-field" />
+                <input type="text" placeholder="Top text" className="meme-text-field" />
+                <input type="text" placeholder="Bottom text" className="meme-text-field" />
                 <button type="submit" className="meme-btn">Get a new meme image 
                     <AiOutlinePicture className="btn-icon"/>
                 </button>
