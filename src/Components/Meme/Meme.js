ff872fd2b8c99bbd820077772a4ebf2ad1bcfcc9
Marlon Leon <ghostveene@gmail.com>
2023-01-20 09:44:40 -0500

    removed type for btn

--- src/Components/Meme/Meme.js
+++ src/Components/Meme/Meme.js
@@ -8,7 +8,7 @@ export default function Meme() {
             <div className="meme-input-section">
                 <input type="text" placeholder="Top text" className="meme-text-field" />
                 <input type="text" placeholder="Bottom text" className="meme-text-field" />
-                <button type="submit" className="meme-btn">Get a new meme image 
+                <button className="meme-btn">Get a new meme image 
                     <AiOutlinePicture className="btn-icon"/>
                 </button>
             </div>
