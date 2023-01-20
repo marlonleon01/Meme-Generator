8c0402f489b187892529cd041d58bb457c9ba936
Marlon Leon <ghostveene@gmail.com>
2023-01-20 07:16:26 -0500

    added all elements for header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,14 @@
 import "./Header.css"
+import trollFace from "../../imgs/Troll Face.png"
 
 export default function Header() {
     return (
-        <div>
-            <h1>Hello World</h1>
+        <div className="header">
+            <div className="title-container">
+                <img src={trollFace} className="troll-face"></img>
+                <h1 className="header-title">Meme Generator</h1>
+            </div>
+            <h3 className="header-course-description">React Course-Project 3</h3>
         </div>
     )
 }
