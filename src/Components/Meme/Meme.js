23ac68bde130a4c7add3049ab274f2ca07d95b3c
Marlon Leon <ghostveene@gmail.com>
2023-01-20 09:01:41 -0500

    added all elements with class names

--- src/Components/Meme/Meme.js
+++ src/Components/Meme/Meme.js
@@ -1,8 +1,15 @@
+import {AiOutlinePicture} from "react-icons/ai"
+
 import "./Meme.css"
 
 export default function Meme() {
     return (
-        <div>
-        </div>
+        <form className="meme-container">
+            <input type="text" placeholder="Shut up" className="meme-text-field"></input>
+            <input type="text" placeholder="and take my money" className="meme-text-field"></input>
+            <button type="submit" className="meme-btn">Get a new meme image 
+                <AiOutlinePicture className="btn-icon"/>
+            </button>
+        </form>
     )
 }
